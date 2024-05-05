2bc7e0d5468c5bc683d17a03c3146ca85d41d1dc
kafpliz <kafpliz@gmail.com>
2024-05-06 00:27:17 +0300

    мелкое обновление базы данных и приёма анкет

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 import express from "express"
 import { FormRouter } from "./routes/form.route";
 import { AuthRoute } from "./routes/auth.route";
-
+import cors from 'cors'
 
 const app = express();
 
-
-
+app.use(cors())
+app.use(express.static('static'))
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Work')
--- routes/form.route.ts
+++ routes/form.route.ts
@@ -17,17 +17,20 @@ connection.connect((err) => {
 
 
 FormRouter.post('/create', async (req, res) => {
+    console.log(req.body);
+        
+  
     let body = req.body;
     let formType: number = body.human == true ? 1 : 0
 
 
 
     if (formType == 1) {
-        let sqlHuman: string = 'insert into human_forms(human, firstName, lastName,  peculiarity, description, img, time,found)';
-        let valueHuman: string = ` values("${body.human == true ? 1 : 0}", "${body.firstName}", "${body.lastName}"," ${body.peculiarity}", "${body.description}", "${body.img}","${body.date}", "${body.found == true ? 1 : 0}")`
+        let sqlHuman: string = 'insert into human_forms(human, firstName, secondName, lastName, peculiarity, description, img, time,found)';
+        let valueHuman: string = ` values("${body.human == true ? 1 : 0}", "${body.firstName}","${body.secondName}", "${body.lastName}"," ${body.peculiarity}", "${body.description}", "${body.img}","${body.date}", "0")`
 
-        connection.execute(sqlHuman + valueHuman, (err, result, fields) => { console.log(err); })
-        connection.execute('select * from human_forms', (err, result, fields) => { res.send(result) })
+        connection.execute(sqlHuman + valueHuman, (err, result, fields) => {err? console.log(err): res.status(200).json({message: 'успешно', result})})
+  
        
 
     }
@@ -52,9 +55,9 @@ FormRouter.get('/get', async (req, res) => {
     console.log(query);
 
     if (query.type == 'human') {
-        connection.execute('select * from human_forms', (err, result, fields) => { res.send(result) })
+        connection.execute('select * from human_forms', (err, result, fields) => { res.json(result) })
     } else if (query.type == 'animal') {
-        connection.execute('select * from animals_forms', (err, result, fields) => { res.send(result) })
+        connection.execute('select * from animal_forms', (err, result, fields) => { res.json(result) })
     } else if (!query.type) {
         connection.execute('SELECT * FROM human_forms', (err, humanResult: any, fields) => {
             if (err) { console.log(err); return }
@@ -63,14 +66,14 @@ FormRouter.get('/get', async (req, res) => {
 
             }
 
-            connection.execute('SELECT * FROM animals_forms', (err, animalsResult: any, fields) => {
+            connection.execute('SELECT * FROM animal_forms', (err, animalsResult: any, fields) => {
                 if (err) { console.log(err); return }
 
                 for (let i = 0; i < animalsResult.length; i++) {
                     arr.push(animalsResult[i])
                 }
 
-                res.send(arr);
+                res.json(arr);
             });
 
         });
